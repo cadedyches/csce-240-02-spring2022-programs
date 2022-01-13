b822d3394e132b0f75ab8b1fc4d4d11d39213797
cadedyches <67021259+cadedyches@users.noreply.github.com>
2022-01-12 20:14:34 -0500

    Add files via upload

---
+++ C++HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 12, 2022
+ *      Author: USER
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	return 0;
+}
